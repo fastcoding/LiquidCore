9cd4c7b12466249e99b19154e6810e9ace29b79b
zhang han xi <zhanghx@szsuma.com>
2018-07-07 01:54:21 +0800

    update build process

--- deps/node-8.9.3/build.sh
+++ deps/node-8.9.3/build.sh
@@ -5,16 +5,26 @@ else
    ARCH=$1
 fi
 
+if [ -z "$BUILDTYPE" ]; then
+	bt=Release
+  S=
+else
+  bt=Debug
+	S=_g
+fi
+
+tgt=libnode${S}.so
+
 HOSTDIR=out-backup/host
 OUTDIR=out-backup/$ARCH
 
 mkdir -p $OUTDIR
 
 build_host() {
-   if ./configure && make; then 
+   if ./configure && make BUILDTYPE=$bt; then 
  			mkdir -p $HOSTDIR
-      cp out/Release/obj.target/libnode.so  $HOSTDIR
-			cp out/Release/mksnapshot	$HOSTDIR
+      cp out/$bt/obj.target/$tgt  $HOSTDIR
+			cp out/$bt/mksnapshot${S}	$HOSTDIR
 	 fi
 }
 
@@ -45,27 +55,36 @@ if ! adb shell "uname -a"|grep -qe $UNAME;  then
    echo emulator for $ARCH is not running!
    exit 1
 fi
-SNAPSHOT=out/Release/mksnapshot
-if ! make -j4; then
+SNAPSHOT=out/$bt/mksnapshot$S
+if ! make -j4 BUILDTYPE=$bt; then
    if ! [ -e  $SNAPSHOT ]; then
       echo compile failed!
       exit 1
    fi
    cp $SNAPSHOT $OUTDIR
-   cp $HOSTDIR/mksnapshot out/Release/
-   if ! make; then
+   #always copy release version
+   cp $HOSTDIR/mksnapshot out/$bt/
+   if ! make BUILDTYPE=$bt; then
       echo failed in second make
       exit 1
    fi
-   adb root
+   adb root || echo "failed to adb root " && exit 1
+	 OK=$(adb shell "test -e /data/libc++_shared.so && echo ok")
+   if [ "$OK" != "ok" ]; then
+      adb push $NDK/std-toolchains/android-14-arm-libc++/arm-linux-androideabi/lib/armv7-a/libc++_shared.so /data
+   fi
 	 adb push $OUTDIR/mksnapshot /data
-	 adb shell "cd /data;./mksnapshot --startup_src=snapshot.cc;exit"
-   adb pull /data/snapshot.cc out/Release/obj.target/v8_snapshot/geni/
-	 touch out/Release/obj.target/v8_snapshot/geni/snapshot.cc
-	 if ! make;then 
+	 adb shell "export LD_LIBRARY_PATH=/data:/system/lib;cd /data;./mksnapshot --startup_src=snapshot.cc;exit"
+   adb pull /data/snapshot.cc out/$bt/obj.target/v8_snapshot/geni/
+	 touch out/$bt/obj.target/v8_snapshot/geni/snapshot.cc
+	 if ! make BUILDTYPE=$bt;then 
       echo make failed
       exit 1
    fi
-   cp out/Release/obj.target/libnode.so $OUTDIR
+   cp out/$bt/obj.target/$tgt $OUTDIR
+	 if [ -d out.$ARCH ]; then
+      rm -rf out.$ARCH
+      mv out out.$ARCH
+   fi
 fi
 
